628b64d9c2c12e0ce08f521106391ccb26b8c807
Lumoyoo <1504734652@qq.com>
2025-10-14 21:39:47 +0800

    refactor(Home): 将HomeBody组件内容直接整合到Home页面
    移除独立的HomeBody组件，将其模板和样式直接移至Home.vue文件中
    简化组件结构，减少不必要的组件层级

--- pages/Home.vue
+++ pages/Home.vue
@@ -1,5 +1,10 @@
 <template>
-  <HomeBody></HomeBody>
+  <div class="home">
+    <div class="home-title">
+      Welcome to Sun's blog!
+    </div>
+    <div class="home-sub-title">I'am a frontEnd developer</div>
+  </div>
   <HomeMain :thingList="(thingList as Array<BlogType>)"></HomeMain>
 </template>
 
@@ -21,4 +26,29 @@ onBeforeMount(() => {
 })
 </script>
 
-<style scoped lang="scss"></style>
+<style scoped lang="scss">
+.home {
+  width: 100%;
+  height: 100vh;
+  position: relative;
+  z-index: 21;
+  text-align: center;
+  padding-top: 38vh;
+  box-sizing: border-box;
+  background-color: #fff;
+
+  .home-title {
+    font-size: 2em;
+    font-family: Georgia, "Helvetica Neue", Times, serif;
+    user-select: none;
+  }
+
+  .home-sub-title {
+    font-family: Georgia, "Helvetica Neue", Times, serif;
+    font-style: italic;
+    font-size: 1.3em;
+    padding: 15px;
+    user-select: none;
+  }
+}
+</style>
