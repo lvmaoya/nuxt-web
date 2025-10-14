a1b855a439c1efea7d3818afce70c0b7225ff4e4
sunjianxiang <1504734652@qq.com>
2025-10-14 17:01:10 +0800

    feat(页面): 添加动态路由404页面并更新NotFound样式
    为动态路由添加404捕获页面，并更新NotFound组件的样式以改善视觉效果

--- pages/NotFound.vue
+++ pages/NotFound.vue
@@ -1,7 +1,17 @@
 <template>
-  <div>没有找到页面！</div>
+  <div class="inner">没有找到页面！</div>
 </template>
 
 <script setup></script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+  div {
+    border-bottom: 1.2px solid rgb(189, 188, 188);
+    height: 100vh;
+    padding-top: 150px;
+    margin-bottom: 258px;
+    background-color: #fff;
+    position: relative;
+    z-index: 21;
+  }
+</style>
