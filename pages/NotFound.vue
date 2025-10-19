f790d348fdb334fde4b9632e09f1a70385da38f9
Lumoyoo <1504734652@qq.com>
2025-10-19 15:45:38 +0800

    feat(NotFound): 添加返回首页链接并调整样式布局
    在未找到页面中添加返回首页的链接，并调整布局样式为弹性布局以更好地展示内容

--- pages/NotFound.vue
+++ pages/NotFound.vue
@@ -1,17 +1,31 @@
 <template>
-  <div class="inner">没有找到页面！</div>
+  <div class="inner">
+    没有找到页面！
+    <a href="/" class="back-home">返回首页</a>
+  </div>
 </template>
 
 <script setup></script>
 
 <style lang="scss" scoped>
-  div {
-    border-bottom: 1px solid var(--primary-border-color);
-    height: 100vh;
-    padding-top: 150px;
-    margin-bottom: 258px;
-    background-color: #fff;
-    position: relative;
-    z-index: 21;
-  }
+div {
+  border-bottom: 1px solid var(--primary-border-color);
+  height: 100vh;
+  padding-top: 150px;
+  margin-bottom: 258px;
+  background-color: #fff;
+  position: relative;
+  z-index: 21;
+  display: flex;
+  justify-content: flex-start;
+  flex-direction: column;
+  align-items: flex-start;
+  gap: 32px;
+}
+
+.back-home {
+  font-size: var(--text-font-size);
+  color: var(--primary-title-color);
+  border-bottom: 1px solid var(--primary-title-color);
+}
 </style>
