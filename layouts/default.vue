ba4894d1ef5e669335dcb0e1223b4bbdfe2a362c
sunjianxiang <1504734652@qq.com>
2025-11-06 11:42:30 +0800

    feat: 添加@lvmaoya/chat-bot依赖并集成聊天组件

--- layouts/default.vue
+++ layouts/default.vue
@@ -3,9 +3,13 @@
     <NavBar></NavBar>
     <slot></slot>
     <Footer></Footer>
+    <ClientOnly>
+        <ChatWidget botName="Lvmaoya" endpointUrl="http://localhost:8080/h5/chat/rag/stream" :greetings="['您好，请问有什么可以帮到您？']" :questions="[]" placeholder="请输入您的问题" />
+    </ClientOnly>
   </div>
 </template>
 
 <script setup>
 import 'animate.css';
+import { ChatWidget } from '@lvmaoya/chat-bot'
 </script>
